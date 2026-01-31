27cd7f40c4a613393da5824851c5dc193321640d
Sejal jain <sejaljain0009@gmail.com>
2026-01-31 18:03:25 +0530

    master-stack

--- script.js
+++ script.js
@@ -57,10 +57,56 @@ obj.age=25;
 console.log(obj.age);*/
 
 //functions return :
-function abcd()
+/*function abcd()
 {
     return 12;
 }
 var ans=abcd();
-console.log(ans);
+console.log(ans);*/
 
+
+
+//sync and async kya hota hai ?
+//async js kya hai ?
+//js is not asynchronous
+//async ki poori kahani 
+//single threading and multi threading
+//callbacks
+//promises
+//then and catch
+//try and catch
+//async and await
+// 5 use cases ekdum real world wale
+//[concept aside]
+//concurrency and parallelism
+//throttling
+
+
+//synchronous - simple code
+/*console.log("hi");
+console.log("hi 2");
+console.log("hi 3");
+console.log("hi 4");*/
+//ismein line-wise sare print ho jayege
+
+//synchronous in functions
+/*console.log("hi");
+setTimeout(function(){},2000);
+console.log("hi 2");*/
+//ismein dono lines print ho jayegi aur "hi 2" sath k sath print ho jayega hi k baad hi beech wali line apna kaam krti rhe parallely.
+
+//asynchronous functions
+/*console.log("hi");
+setTimeout(function(){
+    console.log("hi 2");
+},2000);*/
+//"hi" print ho kr fir 2000 second k baad "hi 2" print hoga uske andr . alg se kuch ni chlega
+
+//Main-Stack and Side-Stack concept
+/*console.log("hi");
+console.log("hi 2");
+setTimeout(function(){
+    console.log("hi 3");
+},0);
+console.log("hi 4");*/
+//pehle sare sync pass hue fir main khali hogya fir usne side ko check kiya
