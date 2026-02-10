ac8bf237c07baad90013584682854a94cd40c519
Sejal jain <sejaljain0009@gmail.com>
2026-02-10 12:23:02 +0530

    modules and functions

--- Node-Js/hello.js
+++ Node-Js/hello.js
@@ -1,3 +1,13 @@
-console.log('hi there ! I am JS');
+//lecture - 3
+//console.log('hi there ! I am JS'); - it will run in both browser and node js
 //console.log(window); - eh ni run honge ehde ch
 //console.log(alert("hi")); - eh v ni run hona ehde ch
+
+//lecture - 4
+//const math = require("./math.js");
+//console.log(math.add(9,7));
+//console.log(math.sub(9,7));
+
+//const math=require("./fs");//yeh write krne se error ayega kyuki currently it is not available
+const math=require("fs");//is se aa jayega kyuki by default hota hai fs - file system module
+console.log(math);
---
+++ Node-Js/math.js
@@ -0,0 +1,9 @@
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+module.exports = {add, sub};
+/*exports.mul= (a,b)=> a*b; */ //another way to export functions
