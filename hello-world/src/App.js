c417e62b20c25530d3e341eaebb0a37f49328af0
Sejal jain <sejaljain0009@gmail.com>
2026-02-25 10:53:38 +0530

    working and understanding components of react js

---
+++ hello-world/src/App.js
@@ -0,0 +1,10 @@
+import Button from './Components/Button';
+function App() {
+  return (
+    <div className="App"> 
+    <h1> hello from react app </h1> 
+    <Button/>
+    </div>     
+  );
+}
+export default App;
---
+++ hello-world/src/Components/Button.jsx
@@ -0,0 +1,4 @@
+const Button=()=>{
+    return <button>Click Me I am a Custom Button Component </button>
+}
+export default Button;
